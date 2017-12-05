84507a4935dcaa9b36d839ff2d63c125f5d80def
RoMalms10 <156@holbertonschool.com>
2017-12-05 22:19:20 +0000

    Create python script that evaluates last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of", number, "is", end=" ")
+if number < 0:
+    number *= -1
+print((number % 10), "and is", end=" ")
+if (number % 10) > 5:
+    print("greater than 5")
+elif (number % 10) == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
