8ce8e7ddab5f546bf969ccd3cd30cc28ba2ca1fa
RoMalms10 <156@holbertonschool.com>
2017-12-05 23:36:52 +0000

    Create python function that prints capitalized string

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+	for i in range(len(str)):
+		check = ord(str[i])
+		if check >= 97 and check <= 122:
+			check -= 32
+		check = chr(check)
+		print(check, end="")
+	print()
