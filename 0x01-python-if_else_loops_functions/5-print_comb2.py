258359167ad4e565c520a8d99c06a7e0f1296fdd
RoMalms10 <156@holbertonschool.com>
2017-12-06 01:13:02 +0000

    Convert tabs to spaces

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-	if i == 99:
-		break
-	print("{:02}, ".format(i), end="")
+    if i == 99:
+        break
+    print("{:02}, ".format(i), end="")
 print(i)
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-	x = i + 1
-	if x == 9:
-		break
-	while (x < 10):
-		print("{}{}, ".format(i, x), end="")
-		x += 1
+    x = i + 1
+    if x == 9:
+        break
+    while (x < 10):
+        print("{}{}, ".format(i, x), end="")
+        x += 1
 print("{}{}".format(i, x))
