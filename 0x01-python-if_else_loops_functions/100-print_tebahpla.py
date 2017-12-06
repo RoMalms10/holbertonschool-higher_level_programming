284cd77597b871c998ab8c7312bd1a173f342710
RoMalms10 <156@holbertonschool.com>
2017-12-06 03:32:03 +0000

    Correct print statements

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print(chr(i))
+        print("{}".format(chr(i)), end ="")
     else:
-        print(chr(i - 32))
+        print("{}".format(chr(i - 32)), end="")
