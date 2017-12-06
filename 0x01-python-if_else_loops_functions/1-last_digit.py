eec16920dd04138881bd72e6567b3f2d420604d6
RoMalms10 <156@holbertonschool.com>
2017-12-06 01:44:23 +0000

    Correct negative number statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 print("Last digit of", number, "is", end=" ")
+
 if number < 0:
-    number *= -1
-    print(-(number % 10), "and is", end=" ")
+    lastdigit = (abs(number) % 10) * -1
 else:
-    print((number % 10), "and is", end=" ")
-if (number % 10) > 5:
+    lastdigit = number % 10
+print(lastdigit, "and is", end=" ")
+if lastdigit > 5:
     print("greater than 5")
-elif (number % 10) == 0:
+elif lastdigit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
