74cdf4933bd7f91b887578bfeb959bb81d1ba511
RoMalms10 <156@holbertonschool.com>
2017-12-06 01:18:19 +0000

    Convert tabs to spaces

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	last_digit = number
-	if last_digit < 0:
-		last_digit *= -1
-	print((last_digit % 10), end="")
-	return (last_digit % 10)
+    last_digit = number
+    if last_digit < 0:
+        last_digit *= -1
+    print((last_digit % 10), end="")
+    return (last_digit % 10)
