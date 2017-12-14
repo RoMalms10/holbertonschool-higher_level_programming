dfe5ec0de27539f602e32ab97525e1df0ab882f7
RoMalms10 <156@holbertonschool.com>
2017-12-14 23:09:15 +0000

    get rid of list before map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [[] for i in range(len(matrix))]
     for i in range(len(matrix)):
-        new_matrix[i] += list(map((lambda x: x ** 2), matrix[i]))
+        new_matrix[i] += map((lambda x: x ** 2), matrix[i])
     return new_matrix
