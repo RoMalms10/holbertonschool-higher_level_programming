ec4393da5a7ae5b4d8f6869abf2979827c17440c
RoMalms10 <156@holbertonschool.com>
2017-12-15 22:07:22 +0000

    Create python function that returns weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    numerator = 0
+    denominator = 0
+    for tup in my_list:
+        numerator += (tup[0] * tup[1])
+        denominator += tup[1]
+    return numerator / denominator
