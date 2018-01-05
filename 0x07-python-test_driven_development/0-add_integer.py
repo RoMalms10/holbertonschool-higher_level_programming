6c7d27d2801805ba697ecd6c89d76c17e95a653e
RoMalms10 <156@holbertonschool.com>
2018-01-05 22:14:07 +0000

    Create module that adds integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""My addition module"""
+
+def add_integer(a, b):
+    """Returns a + b"""
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
