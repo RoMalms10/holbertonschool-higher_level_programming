124a36fd8ca574e2802c3d046de155c6c7af2b6c
RoMalms10 <156@holbertonschool.com>
2018-01-05 00:16:57 +0000

    Fix pep8

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,7 +2,7 @@
 class Node:
     """ Creates a new node
     """
-    
+
     def __init__(self, data, next_node=None):
         """ Initializes the information in the node
 
@@ -43,9 +43,10 @@ class Node:
     def next_node(self, value):
         """ Setter - sets the value for next_node
         """
-        if value != None and not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """ Creates a singly linked list
@@ -65,10 +66,10 @@ class SinglyLinkedList:
         """
         new_str = ""
         pr = self.__head
-        while pr != None:
+        while pr is not None:
             new_str += str(pr.data)
             pr = pr.next_node
-            if pr != None:
+            if pr is not None:
                 new_str += "\n"
         return new_str
 
@@ -82,15 +83,15 @@ class SinglyLinkedList:
         self.new_node = Node(value, None)
         temp = self.__head
         # Checks if there is no list
-        if self.__head == None:
-           self.__head = self.new_node
+        if self.__head is None:
+            self.__head = self.new_node
         # Checks if the first value is bigger than the new node value
         elif temp.data > value:
             self.new_node.next_node = self.__head
             self.__head = self.new_node
         # Goes through the list to see where to add the new node
         else:
-            while temp.next_node != None:
+            while temp.next_node is not None:
                 if temp.next_node.data > value:
                     break
                 temp = temp.next_node
