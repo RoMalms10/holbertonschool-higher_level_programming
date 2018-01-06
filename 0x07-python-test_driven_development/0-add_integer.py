2f87efe5c62cf53ee3ea136b7fdd3ca175d3e3e6
RoMalms10 <156@holbertonschool.com>
2018-01-06 06:21:42 +0000

    Update Doc String

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""My addition module"""
+"""My addition module
+
+add_integer: adds two integers together
+
+"""
 
 def add_integer(a, b):
-    """Returns a + b"""
+    """Returns a + b
+    Args: a and b (int): the numbers to add
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
