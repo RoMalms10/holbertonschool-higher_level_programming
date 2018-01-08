c85d42475ff8f3dc75cf2e376494f2a81343925c
RoMalms10 <156@holbertonschool.com>
2018-01-08 23:19:40 +0000

    Correct when no punctuation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -20,4 +20,4 @@ def text_indentation(text):
             print(new_str.strip(" "))
             new_str = ""
     if letter not in ch:
-        print(new_str.strip(" "))
+        print(new_str.strip(" "), end="")
