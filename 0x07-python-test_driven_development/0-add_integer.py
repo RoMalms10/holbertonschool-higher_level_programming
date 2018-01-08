e2cebf6d071f8eb72822df3482f532c858982d67
RoMalms10 <156@holbertonschool.com>
2018-01-08 01:20:12 +0000

    Clean up isinstance statements

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,8 +9,8 @@ def add_integer(a, b):
     """Returns a + b
     Args: a and b (int): the numbers to add
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
