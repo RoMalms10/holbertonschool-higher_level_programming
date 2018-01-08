81f5c372f1430787f8f57057b9a533e3cb192d55
RoMalms10 <156@holbertonschool.com>
2018-01-08 01:22:51 +0000

    Add Docstrings

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,24 +5,46 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """
+    """Tests the function max_integer for correct output
     """
 
     def test_integer(self):
-        """
+        """Tests all positive numbers
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_None(self):
-        """
+        """Tests what happens when None is passed
         """
         with self.assertRaises(TypeError):
             max_integer(None)
 
     def test_negatives(self):
-        """
+        """Tests a list of negative numbers
         """
         self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+
+    def test_zero(self):
+        """Tests a list size of 0
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_not_integer(self):
+        """Tests when list contains something that's not an integer
+        """
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, "Penguin"])
+
+    def test_positive_and_negative(self):
+        """Test a list with positive and negative numbers
+        """
+        self.assertEqual(max_integer([1,6, 100, 4, 0, -1, 10]), 100)
+
+    def test_not_a_list(self):
+        """Test a non-list item
+        """
+        with self.assertRaises(TypeError):
+            max_integer("not a list")
 
 if __name__ == '__main__':
     unittest.main()
