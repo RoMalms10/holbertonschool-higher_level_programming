f6cecac5159f21eed162d1268984d7ac1118d8a9
RoMalms10 <156@holbertonschool.com>
2018-01-17 23:32:31 +0000

    Create program to append to files

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" My module for writing to files
+"""
+
+
+def write_file(filename="", text=""):
+    """ Appends text to the end of a file
+
+    Args:
+        filename (str): the file to append to
+        text (str): the text to append
+    """
+    with open(filename, mode='a', encoding="UTF=8") as f:
+        return(f.write(text))
