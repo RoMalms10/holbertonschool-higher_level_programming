bb2d740970fc6627a3bca2579a45522b9a58a19b
RoMalms10 <156@holbertonschool.com>
2018-01-18 00:45:55 +0000

    Create program that loads JSON into Python

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My module for converting from JSON
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Converts JSON string into a Python object
+
+    Args:
+        my_str (str): a JSON string to convert
+    """
+    return(json.loads(my_str))
