516114c4fbd3ac72ee98b401a668a8af8bc13cb5
RoMalms10 <156@holbertonschool.com>
2018-01-19 23:21:36 +0000

    Add blank lines after text

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,12 @@
 class MyList(list):
     """ Class that inherits from list
     """
+
     def print_sorted(self):
         """ Prints self sortedly, without modifying the
             original list
         """
+
         new_list = self[:]
         new_list.sort()
-        print (new_list)
+        print(new_list)
