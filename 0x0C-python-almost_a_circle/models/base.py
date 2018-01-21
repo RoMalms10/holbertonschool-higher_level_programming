932ab09df762ec1c435ededa843f7ae8895b9222
RoMalms10 <156@holbertonschool.com>
2018-01-21 06:17:33 +0000

    Update class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ My Module for creating a base class
 """
+import json
 
 
 class Base:
@@ -26,3 +27,31 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Turns the dictionary representation of an instance into JSON
+
+        Args:
+            list_dictionaries (list): list of representations of an instances
+        """
+
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        """
+
+        filename = "{}.json".format(cls.__name__)
+        obj_list = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                obj_list += [obj.to_dictionary()]
+        
+        json_list = Base.to_json_string(obj_list)
+
+        with open(filename, "w+", encoding="UTF-8") as f:
+            f.write(json_list)
+            
