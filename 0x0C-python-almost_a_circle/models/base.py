6fc1bed7f425014d5e72e52b414bbccd750d5c2c
RoMalms10 <156@holbertonschool.com>
2018-01-22 04:44:17 +0000

    Update Base to the end

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,11 +36,32 @@ class Base:
             list_dictionaries (list): list of representations of an instances
         """
 
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the Python version of a JSON string
+
+        Args:
+            json_string (str): string to turn into Python object
+        """
+
+        if json_string is None or len(json_string) == 0:
+            json_string = []
+
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
+        """ Loads a list of objects, turns those objects into a list of
+            dictionaries then sends them to a function that turns them into
+            a JSON string, then saves it to a file, based on the name of a
+            class.
+
+        Args:
+            list_objs (list of objects): objects to turn into 
         """
 
         filename = "{}.json".format(cls.__name__)
@@ -54,4 +75,39 @@ class Base:
 
         with open(filename, "w+", encoding="UTF-8") as f:
             f.write(json_list)
-            
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Creates a new instance of Square or Rectangle based off dictionary
+
+        Args:
+            dictionary (dict): dictionary of values to make into an instance of
+                               Square or Rectangle
+        """
+
+        if "size" in dictionary:
+            c1 = cls(1)
+        else:
+            c1 = cls(1, 1)
+
+        c1.update(**dictionary)
+
+        return c1
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        """
+
+        from_filename = "{}.json".format(cls.__name__)
+        instance_list = []
+
+        try:
+            with open(from_filename, mode="r+", encoding="UTF-8") as f:
+                raw_json = f.read()
+            list_of_dicts = cls.from_json_string(raw_json)
+            for d in list_of_dicts:
+               instance_list += [cls.create(**d)]
+        except FileNotFoundError:
+            pass
+        return instance_list
