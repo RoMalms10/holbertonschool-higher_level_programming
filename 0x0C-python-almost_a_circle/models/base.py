489783a9a1b600eebdc8f3061a607f75a6139d0c
RoMalms10 <156@holbertonschool.com>
2018-01-24 02:13:18 +0000

    Update method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,7 @@ class Base:
         """
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            list_dictionaries = "[]"
+            list_dictionaries = []
         return json.dumps(list_dictionaries)
 
     @staticmethod
