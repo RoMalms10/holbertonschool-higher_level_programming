a24c2b380ea5f419622c2bb7b0119643637f4f38
RoMalms10 <156@holbertonschool.com>
2018-03-01 03:42:57 +0000

    Update script to only contain one SELECT

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,9 @@
 -- Shows which genres are tagged for Dexter
 SELECT tv_genres.name AS name
 FROM tv_show_genres
-LEFT OUTER JOIN tv_genres
+INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id =
-    (SELECT id
-     FROM tv_shows
-     WHERE title = "Dexter")
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
 ORDER BY name ASC;
