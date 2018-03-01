fe6b18349ea5cb689653844f07412f48b9747857
RoMalms10 <156@holbertonschool.com>
2018-03-01 02:41:14 +0000

    List all tv shows that have no genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in the hbtn_0d_tvshows database that have no genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT OUTER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
