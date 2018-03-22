293b53415681e4de95d6af36f9ac348e8b873394
RoMalms10 <156@holbertonschool.com>
2018-03-22 23:29:47 +0000

    Print out the cities in a state using user input
    Look through a TABLE to find cities in a state specified by the user

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' This module queries cities TABLE and joins it with states
+    and sorts by input
+'''
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name \
+                 FROM cities \
+                 INNER JOIN states \
+                 ON cities.state_id = states.id \
+                 WHERE states.name=%s", (argv[4], ))
+    query = cur.fetchall()
+    for row in range(len(query) - 1):
+        print(query[row][0], end=", ")
+    print(query[row + 1][0])
+    cur.close()
+    db.close()
