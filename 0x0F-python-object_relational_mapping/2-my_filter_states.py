43be1080a9b593dc04b60da376e71204e70fe204
RoMalms10 <156@holbertonschool.com>
2018-03-22 22:00:47 +0000

    Use MySQLdb to query a TABLE based off input
    Take a 4th argument on the command line for filtering

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' This module queries a TABLE and filters out information based on input'''
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                 FROM states \
+                 WHERE name \
+                 LIKE '{}' \
+                 ORDER BY id ASC".format(argv[4]))
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+    cur.close()
+    db.close()
