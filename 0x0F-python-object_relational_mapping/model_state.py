491a0e2d213c825cd45056f9fad7935f569364a6
RoMalms10 <156@holbertonschool.com>
2018-03-23 01:32:40 +0000

    Create State Class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module defines a Class State """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Class State that inherits from Base and
+        links to a table named states
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
