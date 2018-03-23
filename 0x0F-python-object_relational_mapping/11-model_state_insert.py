deac17a6a3c9a056ff5e24a9f8df755b88d8d657
RoMalms10 <156@holbertonschool.com>
2018-03-23 03:22:17 +0000

    Add a new state to the current state using SQLAlchemy
    Add new user using session.add(new_state) to add a row to a table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module will connect to a database and access a table"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    for state in session.query(State).order_by(State.id)[-1:]:
+        print("{}".format(state.id))
+    session.close()
