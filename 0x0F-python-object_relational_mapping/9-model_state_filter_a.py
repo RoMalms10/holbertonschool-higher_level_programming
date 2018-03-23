97c1db23063edb67a92e43318db1f0d55007b780
RoMalms10 <156@holbertonschool.com>
2018-03-23 22:34:57 +0000

    Fix pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')):
+    for state in session.query(State).order_by(State.id)\
+            .filter(State.name.like('%a%')):
         print("{}: {}".format(state.id, state.name))
     session.close()
