9bff6480b4e99610ec1206f395251d3a330b9f68
romalms10 <romalms10@gmail.com>
2018-04-04 00:50:31 +0000

    Add -s option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a POST request to a passed URL and displays the body
-curl -X POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
