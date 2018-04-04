0df3248cb772b6d3d06049893138376f995d69c0
romalms10 <romalms10@gmail.com>
2018-04-04 00:49:37 +0000

    Make 3 lines long

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Script that displays the body of a response with header variables
-var="X-HolbertonSchool-User-Id"
-val="98"
-curl -s "$1" -H "$var: $val"
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
