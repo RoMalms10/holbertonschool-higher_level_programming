eb65e14715100cff7333bef702d7ac8bf34275d0
romalms10 <romalms10@gmail.com>
2018-04-09 20:18:15 +0000

    Python script that takes in a URL and sends it a request
    Uses requests and sys packages
    displays the value of the variable X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module that takes in a URL and displays a certain value
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+    req = dict(req.headers)
+    print(req['x-request-id'])
