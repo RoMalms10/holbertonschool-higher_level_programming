ad80348f1f6d90201658d9f9e4f1a48d71f1d822
romalms10 <romalms10@gmail.com>
2018-04-09 17:36:14 +0000

    Python Script that fetches from a URL
    URL is https://intranet.hbtn.io/status and the urllib package was used

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module that fetches from https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type:", type(page))
+    print("\t- content:", page)
+    print("\t- utf8 content:", page.decode("utf-8"))
