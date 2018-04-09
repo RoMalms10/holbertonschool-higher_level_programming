eacaef1591d9777478fb7991b772dc355278fe00
romalms10 <romalms10@gmail.com>
2018-04-09 21:43:59 +0000

    Interact with the Star Wars API
    Pass the Star Wars API a string to search for people in the star wars universe
    Uses the requests and sys packages

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module connects to the Star Wars API
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://swapi.co/api/people/'
+    search = argv[1]
+
+    full_url = url + '?' + 'search={}'.format(search)
+
+    req = requests.get(full_url)
+    data = req.json()
+    
+    print("Number of results: {}".format(data['count']))
+
+    people_list = data['results']
+    for person in people_list:
+        print("{}".format(person['name']))
