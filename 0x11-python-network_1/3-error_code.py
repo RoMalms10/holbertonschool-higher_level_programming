883cb2813daea9ad67454fedc9a423559f554c09
romalms10 <romalms10@gmail.com>
2018-04-09 18:24:54 +0000

    Handles URL Error messages if they happen
    Takes in a URL and tries to open it.
    Used urllib and sys packages

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that takes in a URL and sends a request
+"""
+import urllib.response
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read())
+    except urllib.error.URLError as e:
+        print(e.reason)
