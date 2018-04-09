695bb81ef257a43e86b78a4349e1fe01b3ed8d7a
romalms10 <romalms10@gmail.com>
2018-04-09 20:38:53 +0000

    This python script handles errors if they occur during URL requests
    Uses the sys and requests packages
    Prints out the code if it's above 400

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#1/usr/bin/python3
+""" Module that handles errors if they occur during a URL request
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        req = requests.get(url)
+        print(req.text)
+    except requests.exception.RequestException as e:
+        print(e)
