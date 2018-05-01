55207663f3e725a2186a386596847f8e0f50f8f0
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-05-01 14:49:13 -0700

    Fix python typo

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Create and array of string and print them in a loop
 
-const languages = ['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
