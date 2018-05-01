d8c59c6cbf4360c6759a3d433318a99da744eba6
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-05-01 15:49:12 -0700

    Find second largest number in passed arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Print factorial of a number from the command line using a function
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  let highest = -Infinity;
+  let secondhighest = -Infinity;
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > highest) {
+      secondhighest = highest;
+      highest = process.argv[i];
+    }
+  }
+  console.log(secondhighest);
+}
