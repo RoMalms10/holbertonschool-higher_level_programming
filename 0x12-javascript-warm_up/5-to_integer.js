a7a3452c18b7f780c750acb379a6f43887a2ab74
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-05-01 14:38:33 -0700

    Change var to const

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Converts argument to a number, if it can't convert, it prints 'Not a number'
 
-var myInt = parseInt(process.argv[2]);
+const myInt = parseInt(process.argv[2]);
 
 if (typeof myInt === 'number' && isNaN(myInt) === false) {
   console.log('My number: ' + myInt);
