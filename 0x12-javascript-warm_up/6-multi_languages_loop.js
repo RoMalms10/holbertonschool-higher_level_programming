ac286dc170e276d21ed7e123c24fc5ce8043c2a6
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-05-01 14:48:23 -0700

    Create an array and loop through it

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Create and array of string and print them in a loop
+
+const languages = ['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
