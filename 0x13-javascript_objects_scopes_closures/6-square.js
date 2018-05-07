23e976bb4a541f6a11663ca5d435ece8bf84e2a4
romalms10 <romalms10@gmail.com>
2018-05-07 22:42:32 +0000

    Get rid of call to super.print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -30,12 +30,9 @@ class Square extends Rectangle {
     super(size, size);
   }
   charPrint (c) {
-    if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+    if (c === undefined) c = "X";
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
