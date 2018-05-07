a8d0f192dbc61f4e4ce330c5579ee9ce98691515
romalms10 <romalms10@gmail.com>
2018-05-07 21:29:49 +0000

    Add an if statement for the constructor

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write class Rectangle with width and height
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
