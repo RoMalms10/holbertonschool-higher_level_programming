d73ed7b7da1c94a682e8eb363e716076dde1cd79
romalms10 <romalms10@gmail.com>
2018-05-07 22:53:49 +0000

    Add the require

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,29 +1,7 @@
 #!/usr/bin/node
 // Write class Rectangle with width and height
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    const str1 = 'X';
-    for (let i = 0; i < this.height; i++) {
-      console.log(str1.repeat(this.width));
-    }
-  }
-  rotate () {
-    let holder = this.height;
-    this.height = this.width;
-    this.width = holder;
-  }
-  double () {
-    this.height = 2 * this.height;
-    this.width = 2 * this.width;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
