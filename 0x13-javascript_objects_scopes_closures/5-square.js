ebf9daf330144d7579129a0a2fe49b2f963caf26
romalms10 <romalms10@gmail.com>
2018-05-07 22:29:33 +0000

    Fix semistandard

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,31 +2,31 @@
 // Write class Rectangle with width and height
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-    print () {
-      const str1 = 'X';
-      for (let i = 0; i < this.height; i++) {
-        console.log(str1.repeat(this.width));
-      }
-    }
-    rotate () {
-      let holder = this.height;
-      this.height = this.width;
-      this.width = holder;
-    }
-    double () {
-      this.height = 2 * this.height;
-      this.width = 2 * this.width;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
+  print () {
+    const str1 = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(str1.repeat(this.width));
+    }
+  }
+  rotate () {
+    let holder = this.height;
+    this.height = this.width;
+    this.width = holder;
+  }
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+}
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
 }
