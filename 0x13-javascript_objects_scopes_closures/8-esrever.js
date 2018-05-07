91780331fabe831a095f13f28f3d7f18b93c243d
romalms10 <romalms10@gmail.com>
2018-05-07 23:48:49 +0000

    change to let instead of var

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // Returns the changed version of a list
 
 exports.esrever = function (list) {
-  var changed = [];
+  let changed = [];
   for (let i = list.length - 1; i >= 0; i--) {
     changed.push(list[i]);
   }
