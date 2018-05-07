d9c27b3255639aa848f0fd7040eacdbdae88875e
romalms10 <romalms10@gmail.com>
2018-05-07 23:38:29 +0000

    Reverse a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Returns the reversed version of a list
+
+exports.esrever = function (list) {
+  var reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
+  return reversed;
+};
