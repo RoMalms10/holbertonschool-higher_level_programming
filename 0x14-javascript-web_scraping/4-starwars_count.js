f9f7c2fc3756c0a64bce39133dcb72715f639fda
romalms10 <romalms10@gmail.com>
2018-05-09 02:00:23 +0000

    Find out how many movies Wedge is in

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Check how many times a person appeared in a movie
+
+const request = require('request');
+
+const argv = process.argv.slice(2);
+
+const person = 'https://swapi.co/api/people/18/';
+
+request(argv[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let count = 0;
+    const jsonDict = JSON.parse(body);
+    const listMovies = jsonDict['results'];
+    for (let i = 0; i < listMovies.length; i++) {
+      if (listMovies[i]['characters'].includes(person)) count++;
+    }
+    console.log(count);
+  }
+});
