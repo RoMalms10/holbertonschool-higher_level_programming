ab0177668b76772db8cf5041b0879e0f62dcc5de
romalms10 <romalms10@gmail.com>
2018-05-09 01:39:11 +0000

    Fix semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@ const request = require('request');
 
 const argv = process.argv.slice(2);
 
-request(argv[0], function(err, res, body) {
-    if (err) {
-        console.log(err);
-    } else {
-      console.log('code:', res.statusCode);
-    }
+request(argv[0], function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', res.statusCode);
+  }
 });
