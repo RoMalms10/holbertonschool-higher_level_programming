7a2d41898b7fe0c28a92b90a8bcd33289b6482c2
romalms10 <romalms10@gmail.com>
2018-05-09 01:38:22 +0000

    Check status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const argv = process.argv.slice(2);
+
+request(argv[0], function(err, res, body) {
+    if (err) {
+        console.log(err);
+    } else {
+      console.log('code:', res.statusCode);
+    }
+});
